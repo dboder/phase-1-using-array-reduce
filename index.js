b04382176573ab853b88ed81e7bfb0e92087bd13
Dorsey B <dorseybec@gmail.com>
2023-04-16 17:25:14 -0700

    debug help added

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
     return element + accumulator;
   }, 0); // the 0 represents the inital value for the accumulator if none, it grabs first element and starts at second
+
+/* FOR DEBUGGING ARRAY:
+
+    batteryBatches.forEach(function (e) {
+    console.log("DEBUG: WHAT ARE YOU?!?" + e);
+    });
+*/
